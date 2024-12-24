ede3b8008fd5069f852b368e31bc660b742cb174
Maarten Claes <maarten.claes@student.pxl.be>
2024-12-24 14:27:13 +0100

    US7, US8, US9, a bit of refactoring still necessary

--- backend-java/SpringCloudPXL/messaging-service/src/main/java/be/pxl/services/QueueConfiguration.java
+++ backend-java/SpringCloudPXL/messaging-service/src/main/java/be/pxl/services/QueueConfiguration.java
@@ -1,12 +1,6 @@
 package be.pxl.services;
 
-
-import jakarta.annotation.PostConstruct;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.DefaultClassMapper;
